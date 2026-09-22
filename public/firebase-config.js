26634f4b4cef310a089cbaa6ea7ab3d7f512bd5c
ViTTorZiNnn <118228153+ViTTorZiNnn@users.noreply.github.com>
2026-09-22 15:52:22 -0300

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -8,13 +8,14 @@
 // ============================================================
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-f4K3_aPi_kEy_M3saQu3nt3",
+ apiKey: "AIzaSyCTtxOifcCNB7lObqu1CNwBj288vYwf_zQ",
   authDomain: "mesaquente-d7ec5.firebaseapp.com",
   databaseURL: "https://mesaquente-d7ec5-default-rtdb.firebaseio.com",
   projectId: "mesaquente-d7ec5",
-  storageBucket: "mesaquente-d7ec5.appspot.com",
-  messagingSenderId: "104910232001", 
-  appId: "1:104910232001:web:a1b2c3d4e5f6g7"
+  storageBucket: "mesaquente-d7ec5.firebasestorage.app",
+  messagingSenderId: "609436003177",
+  appId: "1:609436003177:web:a8c051f6b317c727492bf8",
+  measurementId: "G-6BN5NV38PY"
 };
 
 window.MQ_FIREBASE_CONFIG = firebaseConfig;
